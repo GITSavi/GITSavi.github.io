954fb256b754f6ff0b67529e7beb99f03f2e6a9f
GITSavi <p.martin0666@gmail.com>
2016-11-08 16:29:37 -0500

    Delete java.js

--- java.js
+++
@@ -1,5 +0,0 @@
-function change(){
-     var x = document.getElementById('mySelect').value;
-     var y = document.getElementById('mySelect2').value;
-    document.getElementById('total').innerHTML = '$' + (Number(x) + Number(y));
-}
